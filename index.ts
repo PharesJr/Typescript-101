b73dc4da23997049d133871371bb1e0894185bc0
PharesJr <pharesirungu@gmail.com>
2025-05-31 14:23:35 +0300

    Function Parameters Annotations notes added

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-let myNumber = 6
-console.log(myNumber)
+// Here is where you try out the codes
+
+// example of an arrow function that multiplies two numbers:
+
+const multiply = (x: number, y: number) => {
+  return x * y;
+};
+
+const result = multiply(15, 4);
+
+console.log(result);
